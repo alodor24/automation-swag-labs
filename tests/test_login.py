7fabdf48106b53c7230bc351af76c8871c450420
Jose Alejandro <jose-a-mendez@hotmail.com>
2024-06-16 13:32:02 -0300

    Actualización del archivo consts

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,28 +1,27 @@
 from pages.inventory_page import InventoryPage
 from pages.login_page import LoginPage
-from utils.consts import USER_CREDENTIALS
+from utils.consts import USER_CREDENTIALS, URL_PATH
 from selenium import webdriver
 
 class TestLogin:
     # Prueba para el login con usuario estándar
     def test_login(self, driver:webdriver.Remote):
-        driver.get("https://www.saucedemo.com/")
+        driver.get(URL_PATH)
         login_page = LoginPage(driver)
         login_page.login_completo(
             USER_CREDENTIALS["username"]["standard_user"], 
             USER_CREDENTIALS["password"]["secret_sauce"]
         )
-        
         print('Login realizado correctamente...!!')
         
-        driver.get("https://www.saucedemo.com/inventory.html")
+        driver.get(f'{URL_PATH}/inventory.html')
         inventory_page = InventoryPage(driver)
         expect_message = inventory_page.get_title_page()
         print(f'Título de la página de inventario de productos => {expect_message}')
         
     # Prueba para el login con usuario bloqueado
     def test_locked_user(self, driver:webdriver.Remote):
-        driver.get("https://www.saucedemo.com/")
+        driver.get(URL_PATH)
         login_page = LoginPage(driver)
         login_page.login_completo(
             USER_CREDENTIALS["username"]["locked_out_user"], 
@@ -35,7 +34,7 @@ class TestLogin:
         
     # Prueba para el login con credenciales incorrectas
     def test_credentials_error(self, driver:webdriver.Remote):
-        driver.get("https://www.saucedemo.com/")
+        driver.get(URL_PATH)
         login_page = LoginPage(driver)
         login_page.login_completo(
             USER_CREDENTIALS["username"]["malicious_user"], 
--- utils/consts.py
+++ utils/consts.py
@@ -1,6 +1,6 @@
 # Definición de las constantes
 
-URL_PATH = "https://www.saucedemo.com/"
+URL_PATH = "https://www.saucedemo.com"
 
 USER_CREDENTIALS = {
   "username": {
@@ -16,14 +16,4 @@ USER_CREDENTIALS = {
     "secret_sauce": "secret_sauce",
     "failed_password": "failed_password"
   }
-}
-
-PAGES = {
-  "login_page": "login_page",
-  "inventory_page": "inventory_page",
-  "product_page": "product_page",
-  "cart_page": "cart_page",
-  "checkout_step_one_page": "checkout_step_one_page",
-  "checkout_step_two_page": "checkout_step_two_page",
-  "checkout_complete_page": "checkout_complete_page"
 }
