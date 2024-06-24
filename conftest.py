d70e9e163adc25ef84de25214aed0a8c9730402b
Jose Alejandro <jose-a-mendez@hotmail.com>
2024-06-24 18:15:53 -0300

    Script para hacer captura de pantalla en caso se encuentre un fallo

--- conftest.py
+++ conftest.py
@@ -11,3 +11,22 @@ def driver():
     driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
+    
+
+# Script para hacer captura de pantalla en caso se encuentre un fallo
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    """
+    Función para tomar capturas de pantalla en caso de fallo y guardarlas en un archivo de log 
+    llamado failures.log en la carpeta screenshots del directorio raíz del proyecto.
+    """
+    outcome = yield
+    report = outcome.get_result()
+    if report.when == "call" and report.failed:
+        mode = "w" if isinstance(call.excinfo.value, AssertionError) else "a"
+        with open(".failures.log", mode) as f:
+            if "driver" in item.fixturenames:
+                web_driver = item.funcargs["driver"]
+                screenshot_file = f"screenshots/{item.name}.png"
+                web_driver.save_screenshot(screenshot_file)
+                f.write(f"{item.name} failed. Screenshot saved to {screenshot_file}\n")
