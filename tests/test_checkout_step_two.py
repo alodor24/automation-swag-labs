0c31cddb07956908a1d48c3441349010358610db
Jose Alejandro <jose-a-mendez@hotmail.com>
2024-07-15 13:57:02 -0300

    Prueba para comprobar que se encuentra en la página Checkout Step Two

--- tests/test_checkout_step_two.py
+++ tests/test_checkout_step_two.py
@@ -9,6 +9,37 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class TestCheckoutStepTwo:
+  # Prueba para comprobar que se encuentra en la página Checkout Step Two
+  def test_is_checkout_step_two(self, driver: webdriver.Remote):
+    isLoginSuccess = login(driver, STANDARD_USER)
+    
+    if (isLoginSuccess):
+      # Ingresa a la página de inventario de productos y hace click en el carrito de compras
+      WebDriverWait(driver, 10).until(EC.url_to_be(f'{URL_PATH}/inventory.html'))
+      inventory_page = InventoryPage(driver)
+      inventory_page.handle_click_on_shopping_cart()
+      
+      # Redirige a la página del carrito y hace click en el botón Checkout
+      WebDriverWait(driver, 10).until(EC.url_to_be(f'{URL_PATH}/cart.html'))
+      cart_page = CartPage(driver)
+      cart_page.handle_click_on_checkout()
+      
+      # Espera hasta ser redirigido a la primera página del proceso de compra
+      WebDriverWait(driver, 10).until(EC.url_to_be(f'{URL_PATH}/checkout-step-one.html'))
+      
+      # Carga datos en el formulario
+      checkout_step_one_page = CheckoutStepOnePage(driver)
+      checkout_step_one_page.to_do_checkout(FORM_DATA)
+      
+      # Continua a la siguiente pantalla del proceso
+      WebDriverWait(driver, 10).until(EC.url_to_be(f'{URL_PATH}/checkout-step-two.html'))
+      checkout_step_two_page = CheckoutStepTwoPage(driver)
+      
+      # Obtiene el título de la página
+      title_page = checkout_step_two_page.get_title_page()
+      print(f'El título de la página es: {title_page}')
+    
+  
   # Prueba para comprobar el funcionamiento del botón Cancel
   def test_cancel_btn(self, driver: webdriver.Remote):
     isLoginSuccess = login(driver, STANDARD_USER)
